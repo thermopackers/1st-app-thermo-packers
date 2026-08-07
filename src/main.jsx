b86eb1ebe98cb0a807735003bc28854247cf45a4
Pradyumna5777 <chaurasiyapradyumna5955@gmail.com>
2026-08-07 13:40:14 +0530

    Update main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-// import App from "./App";
-// import "./index.css";
-// import { AppProvider } from "./context/AppContext";
-// import { UserProvider } from "./context/UserContext";
-// import { ToDoProvider } from "./context/ToDoContext";
-// import { CaremaxCustomerProvider } from "./context/CaremaxCustomerContext";
+import App from "./App";
+import "./index.css";
+import { AppProvider } from "./context/AppContext";
+import { UserProvider } from "./context/UserContext";
+import { ToDoProvider } from "./context/ToDoContext";
+import { CaremaxCustomerProvider } from "./context/CaremaxCustomerContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
